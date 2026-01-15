ec5b48c6ff6888bc975e71a31caaacfc9c65e1d5
Raheem-O15 <Raheemolabanji15@gmail.com>
2026-01-15 05:46:39 +0000

    Separated Home page into core app; fix Services URL routing

--- services/views.py
+++ services/views.py
@@ -4,3 +4,4 @@ from .service_layer import get_active_services
 def services_list(request):
     services = get_active_services()
     return render(request, "services/services_list.html", {"services": services})
+
